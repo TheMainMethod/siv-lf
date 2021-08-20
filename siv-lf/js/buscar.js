c481aea10d40056bcbc18e67c62eccd50fc3020e
TheMainMethod <mini_neutral_on_windows_live@hotmail.com>
2021-08-19 20:01:03 -0500

    Delete siv-lf directory
    empezando desde cero

--- siv-lf/js/buscar.js
+++
@@ -1,4 +0,0 @@
-function toBusqueda(){
-    location.href = '/siv-lf/busqueda.php';//tal vez hacer que abra una ventana nueva.
-    //Este archivo se utiliza en main para llamar a busqueda.php
-}
--- siv-lf/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//muestra una única cadena, que es el hash de la variable $pass
-
-$pass = 'prueba';
-$hash = Password::hash($pass);
-echo $hash;
-
-?>
